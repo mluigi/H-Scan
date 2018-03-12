df586080dfda4a8e568437da79291fcf556bce30
Luigi Marrandino <luigimarrandino@10-26-48-59.0200.cam.it.iosda.org>
2018-03-12 14:17:27 +0100

    Initial Commit

---
+++ barcodeSuperApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  barcodeSuperApp
+//
+//  Created by Luigi Marrandino on 12/03/18.
+//  Copyright © 2018 Luigi Marrandino. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
