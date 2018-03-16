f4e019dc942eff7065adb01641f540de77e10dbc
Luigi Marrandino <luigimarrandino@cam-08.local>
2018-03-16 10:18:20 +0100

    Added Product info view

--- barcodeSuperApp/RecentViewController.swift
+++ barcodeSuperApp/RecentViewController.swift
@@ -26,16 +26,11 @@ class RecentViewController: UIViewController, SideMenuItemContent {
         showSideMenu()
     }
     
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
     }
-    */
+ 
 }
 
 extension RecentViewController : UITableViewDelegate, UITableViewDataSource {
@@ -46,5 +41,7 @@ extension RecentViewController : UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "RecentCell", for: indexPath)
+//        let product = DB.objects(String.self)[indexPath.row]
+        return cell
     }
 }
