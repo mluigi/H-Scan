1d7abafa70eba0533830050140b4586ee3663048
Alberto Quassone <albertoquassone@cam-13.local>
2018-03-16 11:04:36 +0100

    translation

--- barcodeSuperApp/Prodotti.swift
+++ barcodeSuperApp/Prodotti.swift
@@ -11,34 +11,34 @@ import UIKit
 
 
 
-class Prodotto {
-    var nome: String
-    var immagine: UIImage
-    var calorie: Int
-    var quantità: Int
-    var proprietàNutrizionali: String?
+class Product {
+    var name: String
+    var image: UIImage
+    var calories: Int
+    var pieces: Int
+    var nutritionalProprieties: String?
     
     
-    init(nome: String, immagine: UIImage, calorie: Int, quantità: Int, proprietàNutrizionali: String?) {
-        self.nome = nome
-        self.immagine = immagine
-        self.calorie = calorie
-        self.quantità = quantità
-        self.proprietàNutrizionali = proprietàNutrizionali
+    init(name: String, image: UIImage, calories: Int, pieces: Int, nutritionalProprieties: String?) {
+        self.name = name
+        self.image = image
+        self.calories = calories
+        self.pieces = pieces
+        self.nutritionalProprieties = nutritionalProprieties
     }
     
 }
 
-var MilkaChocoBrownie = Prodotto(nome: "Milka Choco Brownie", immagine: #imageLiteral(resourceName: "milkaChocoBrownie"), calorie: 118*2, quantità: 2, proprietàNutrizionali: nil)
-var KinderBueno = Prodotto(nome: "Kinder Bueno", immagine: #imageLiteral(resourceName: "kinderBueno"), calorie: 122*2, quantità: 2, proprietàNutrizionali: nil)
-var KinderCereali = Prodotto(nome: "barretta Kinder Cereali", immagine: #imageLiteral(resourceName: "kinderCereali"), calorie: 132, quantità: 1, proprietàNutrizionali: nil)
-var KitKat = Prodotto(nome: "Kit-Kat", immagine: #imageLiteral(resourceName: "kitkat"), calorie: 209*4, quantità: 4, proprietàNutrizionali: nil)
-var Bounty = Prodotto(nome: "bounty", immagine: #imageLiteral(resourceName: "bounty"), calorie: 139*2, quantità: 2, proprietàNutrizionali: nil)
-var Mars = Prodotto(nome: "Mars", immagine: #imageLiteral(resourceName: "mars"), calorie: 182, quantità: 1, proprietàNutrizionali: nil)
-var Snickers = Prodotto(nome: "Snickers", immagine: #imageLiteral(resourceName: "snickers"), calorie: 256, quantità: 1, proprietàNutrizionali: nil)
-var KinderColazionePiù = Prodotto(nome: "Kinder Colazione Più (pack of 10 pieces)"
-, immagine: #imageLiteral(resourceName: "kindercolazionepiu"), calorie: 122, quantità: 1, proprietàNutrizionali: nil)
-var Cerealix = Prodotto(nome: "Cerealix (pack of 8 pieces)", immagine: #imageLiteral(resourceName: "cerealix"), calorie: 96, quantità: 1, proprietàNutrizionali: nil)
+var MilkaChocoBrownie = Product(name: "Milka Choco Brownie", image: #imageLiteral(resourceName: "milkaChocoBrownie"), calories: 118*2, pieces: 2, nutritionalProprieties: nil)
+var KinderBueno = Product(name: "Kinder Bueno", image: #imageLiteral(resourceName: "kinderBueno"), calories: 122*2, pieces: 2, nutritionalProprieties: nil)
+var KinderCereali = Product(name: "barretta Kinder Cereali", image: #imageLiteral(resourceName: "kinderCereali"), calories: 132, pieces: 1, nutritionalProprieties: nil)
+var KitKat = Product(name: "Kit-Kat", image: #imageLiteral(resourceName: "kitkat"), calories: 209*4, pieces: 4, nutritionalProprieties: nil)
+var Bounty = Product(name: "bounty", image: #imageLiteral(resourceName: "bounty"), calories: 139*2, pieces: 2, nutritionalProprieties: nil)
+var Mars = Product(name: "Mars", image: #imageLiteral(resourceName: "mars"), calories: 182, pieces: 1, nutritionalProprieties: nil)
+var Snickers = Product(name: "Snickers", image: #imageLiteral(resourceName: "snickers"), calories: 256, pieces: 1, nutritionalProprieties: nil)
+var KinderColazionePiù = Product(name: "Kinder Colazione Più (pack of 10 pieces)"
+, image: #imageLiteral(resourceName: "kindercolazionepiu"), calories: 122, pieces: 1, nutritionalProprieties: nil)
+var Cerealix = Product(name: "Cerealix (pack of 8 pieces)", image: #imageLiteral(resourceName: "cerealix"), calories: 96, pieces: 1, nutritionalProprieties: nil)
 
 
 
@@ -46,15 +46,15 @@ var Cerealix = Prodotto(nome: "Cerealix (pack of 8 pieces)", immagine: #imageLit
 
 
 
-var Mela = Prodotto(nome: "Mela", immagine: #imageLiteral(resourceName: "mela"), calorie: 60, quantità: 1, proprietàNutrizionali: "Aiuta nella prevenzione del cancro, regola i livelli di diabete, abbassa il colesterolo nel sangue")
-var Pera = Prodotto(nome: "Pera", immagine: #imageLiteral(resourceName: "pera"), calorie: 45, quantità: 1, proprietàNutrizionali: "Migliora la digestione, abbassa i livelli di colesterolo nel sangue")
-var Arancia = Prodotto(nome: "Arancia", immagine: #imageLiteral(resourceName: "arancia"), calorie: 50, quantità: 1, proprietàNutrizionali: "Rafforza le difese immunitarie, previene i calcoli renali")
+var Apple = Product(name: "Apple", image: #imageLiteral(resourceName: "mela"), calories: 60, pieces: 1, nutritionalProprieties: "Helps in cancer prevention, regulates diabetes levels, lower cholesterol")
+var Pear = Product(name: "Pear", image: #imageLiteral(resourceName: "pera"), calories: 45, pieces: 1, nutritionalProprieties: "Improves digestion, lowers cholesterol levels in the blood")
+var Orange = Product(name: "Orange", image: #imageLiteral(resourceName: "arancia"), calories: 50, pieces: 1, nutritionalProprieties: "Strengthens the immune system, prevents kidney stones")
 
 
 
-var alternative = [Mela,Pera,Arancia]
+var alternatives = [Apple,Pear,Orange]
 
-var dizionario : [Int:Prodotto] = [
+var dictionary : [Int:Product] = [
     7622210788078:MilkaChocoBrownie,
     80052760:KinderBueno,
     80310266:KinderCereali,
@@ -74,17 +74,15 @@ var dizionario : [Int:Prodotto] = [
 
 
 
-//func sceltaAlternative (_ alternative:[Prodotto]) -> [Prodotto] {
-//    var alternativescelte = [Prodotto]()
+//func pickAnAlternative (_ alternatives:[Product]) -> [Product] {
+//    var pickedalternatives = [Product]()
 //    var index=0
 //    while index < 2 {
-//        let randomnumber = Int(arc4random_uniform(UInt32(alternative.count)+1))
-//        alternativescelte.append(alternative[randomnumber])
+//        let randomnumber = Int(arc4random_uniform(UInt32(alternatives.count)+1))
+//        pickedalternatives.append(alternative[randomnumber])
 //        index += 1
 //    }
-//    return alternativescelte
-//
-//
+//    return pickedalternatives
 //
 //}
 
