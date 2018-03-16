2b0f3a9bc16e66ecbb44e2fe459387a48fce5daf
Alberto Quassone <albertoquassone@cam-13.local>
2018-03-16 12:48:28 +0100

    Update

--- barcodeSuperApp/Prodotti.swift
+++ barcodeSuperApp/Prodotti.swift
@@ -38,11 +38,20 @@ var Snickers = Product(name: "Snickers", image: #imageLiteral(resourceName:"snic
 var KinderColazionePiù = Product(name: "Kinder Colazione Più (pack of 10 pieces)"
         , image: #imageLiteral(resourceName:"kindercolazionepiu"), calories: 122, pieces: 1, nutritionalProprieties: nil)
 var Cerealix = Product(name: "Cerealix (pack of 8 pieces)", image: #imageLiteral(resourceName:"cerealix"), calories: 96, pieces: 1, nutritionalProprieties: nil)
+var Buondi = Product(name:"Buondi Cioccolato (pack of 6 pieces)", image: #imageLiteral(resourceName: "buondi"), calories: 126, pieces: 1, nutritionalProprieties: nil)
+var KinderDelice = Product(name: "Kinder Delice (pack of 10 pieces)", image: #imageLiteral(resourceName: "kinderDelice"), calories: 185, pieces: 1, nutritionalProprieties: nil)
+var CrackersIntegraliMulinoBianco = Product(name: "Crackers Integrali Mulino Bianco (pack of 20 pieces)", image: #imageLiteral(resourceName: "crackers"), calories: 104, pieces: 1, nutritionalProprieties: nil)
+
+
+
+
 
 
 var Apple = Product(name: "Apple", image: #imageLiteral(resourceName:"mela"), calories: 60, pieces: 1, nutritionalProprieties: "Helps in cancer prevention, regulates diabetes levels, lower cholesterol")
 var Pear = Product(name: "Pear", image: #imageLiteral(resourceName:"pera"), calories: 45, pieces: 1, nutritionalProprieties: "Improves digestion, lowers cholesterol levels in the blood")
 var Orange = Product(name: "Orange", image: #imageLiteral(resourceName:"arancia"), calories: 50, pieces: 1, nutritionalProprieties: "Strengthens the immune system, prevents kidney stones")
+
+
 
 
 var alternatives = [Apple, Pear, Orange]
@@ -56,7 +65,10 @@ var dictionary: [Int: Product] = [
     5000159407236: Mars,
     5000159461122: Snickers,
     8000500033715: KinderColazionePiù,
-    8013355998665: Cerealix
+    8013355998665: Cerealix,
+    8014037008634: Buondi,
+    8014037008634: KinderDelice,
+    8076809516655: CrackersIntegraliMulinoBianco
 ]
 
 
