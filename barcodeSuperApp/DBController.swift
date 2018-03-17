07a534fe7b1ed6dcc3dce41d3f5469b959b17fdf
Luigi Marrandino <luigimarrandino@cam-08.local>
2018-03-17 12:47:54 +0100

    Added HealthKit support Added UserDefaults support Updated RecentViewController

--- barcodeSuperApp/DBController.swift
+++ barcodeSuperApp/DBController.swift
@@ -6,7 +6,18 @@
 //  Copyright © 2018 Luigi Marrandino. All rights reserved.
 //
 
-import Foundation
-import RealmSwift
+import UIKit
 
-let DB = try! Realm()
+let DB = UserDefaults()
+
+extension UserDefaults {
+    func saveProduct(product: Product) {
+        var products = array(forKey: "recentProducts") ?? [Product]()
+        products.append(product)
+        set(products, forKey: "recentProducts")
+    }
+    
+    func recentProducts() -> [Product] {
+        return array(forKey: "recentProducts") as! [Product]
+    }
+ }
