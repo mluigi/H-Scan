ca2ad18cdfb01eca5f380560d76ea02f4dd91ef2
Luigi Marrandino <luigimarrandino@cam-08.station>
2018-03-18 15:12:36 +0100

    Updated RecentProducts save logic

--- barcodeSuperApp/AppDelegate.swift
+++ barcodeSuperApp/AppDelegate.swift
@@ -15,6 +15,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        if DB.array(forKey: recentProds) == nil {
+            DB.set([Product](), forKey: recentProds)
+        }
         return true
     }
 
