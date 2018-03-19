34c2e63c81a23a82578de54b92a9d3598c295207
Luigi Marrandino <luigimarrandino@10-26-48-59.0200.cam.it.iosda.org>
2018-03-19 15:42:24 +0100

    Added 2 files

---
+++ barcodeSuperApp/Giornaliero.swift
@@ -0,0 +1,53 @@
+//
+//  Giornaliero.swift
+//  cerchi
+//
+//  Created by Davide Compagnone on 19/03/18.
+//  Copyright © 2018 Davide Compagnone. All rights reserved.
+//
+
+import Foundation
+
+
+
+var peso : Double = 85.0
+var sesso = "M"
+var età : Int = 26
+
+
+func calcoloCalorieGiornaliere () -> (Double) {
+    
+    var risultato : Double = 0
+    
+    if sesso == "M" {
+        // se è uomo
+        switch età {
+        case 17..<29 :  risultato = 15.3 * peso + 679
+        case 29..<59 : risultato = 11.6 * peso + 879
+        case 59..<74 : risultato = 11.9 * peso + 700
+        default : risultato = 8.4 * peso + 819
+            
+        }
+        
+    }
+    else {
+        // se è donna
+        
+        switch età {
+        case 17..<29 :  risultato = 14.7 * peso + 496
+        case 29..<59 : risultato = 8.7 * peso + 829
+        case 59..<74 : risultato = 9.2 * peso + 688
+        default : risultato = 9.8 * peso + 624
+            
+        }
+    }
+    
+    
+    return risultato
+}
+
+
+
+
+var calorieConsumate : Double = 1200.0
+
